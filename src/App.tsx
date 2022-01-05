5a62ddd1040b41725736cc4c89e2c3510bbf9048
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-01-05 21:58:42 +0200

    init react 18 RC clerk demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { SignedIn, UserButton, SignedOut, SignIn } from "@clerk/clerk-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        React 18 RC app
+        <SignedIn>
+          You are signed in <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignIn />
+        </SignedOut>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+// @ts-ignore
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ClerkProvider
+      scriptUrl={"https://js.lclclerk.com/npm/clerk.browser.js"}
+      frontendApi={"clerk.nov0t.k4358.dev.lclclerk.com"}
+    >
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
